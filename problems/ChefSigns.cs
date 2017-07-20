8d4a4a86da31d90aceed7fc6b9897b4c1b85e5d7
RajatChaudhari <silverprince.v1@gmail.com>
2017-07-20 12:00:04 +0530

    leetcodeTwoSumsAccepted

--- problems/ChefSigns.cs
+++ problems/ChefSigns.cs
@@ -13,63 +13,39 @@ namespace problems
         {
             int cases = Convert.ToInt32(Console.ReadLine());
             List<string> signList = new List<string>();
-            int signCount = 0;
-            if (cases >= 1 && cases <= 100000)
+
+            for (int ii = 0; ii < cases; ii++)
             {
-                for (int i = 0; i < cases; i++)
-                {
-                    string sign = Console.ReadLine().Trim(' ');
+                string sign = Console.ReadLine().Trim(' ');
 
-                    if ((sign.Length > 0 && sign.Length <= 100000) && Regex.IsMatch(sign, @"^[<>=]+$"))
+                int l = sign.Length;
+                int[] num = new int[l + 1];
+                num[0] = l + 1;
+                for (int i = 0; i < l; i++)
+                {
+                    if (sign.Substring(i, 1) == ">")
                     {
-                        signCount = signCount + sign.Length;
-                        if (signCount >= 1 && signCount <= 1000000)
-                            signList.Add(sign);
-                        else
-                            break;
+                        num[i + 1] = num[i] + 1;
+                    }
+                    else if (sign.Substring(i, 1) == "<")
+                    {
+                        num[i + 1] = num[i] - 1;
+                    }
+                    else if (sign.Substring(i, 1) == "=")
+                    {
+                        num[i + 1] = num[i];
                     }
                 }
-            }
-                //if (signCount >= 1 && signCount <= 1000000)
-                //{
-                foreach (string s in signList)
-                {
-                    List<int> intlist = new List<int>();
-                    int l = s.Length;
-                    int[] num = new int[l + 1];
-                    num[0] = l + 1;
-                    for (int i = 0; i < l; i++)
-                    {
-                        if (s.Substring(i, 1) == ">")
-                        {
-                            num[i + 1] = num[i] + 1;
-                        }
-                        else if (s.Substring(i, 1) == "<")
-                        {
-                            num[i + 1] = num[i] - 1;
-                        }
-                        else if (s.Substring(i, 1) == "=")
-                        {
-                            num[i + 1] = num[i];
-                        }
-                    }
-                    int P = num.Distinct().Count();
-                for (int z = 0; z < num.Length; z++)
-                {
-                    Console.Write(num[z]);
-
-                }
-                Console.WriteLine("");
+                int P = num.Distinct().Count();
                 Console.WriteLine(P);
-                }
-                //    }
-            //}
+            }
+            
         }
         //static void Main(string[] args)
         //{
         //    ChefSigns cs = new ChefSigns();
         //    cs.CallChefSigns();
-        //    Console.Read();
+        //    Console.ReadLine();
         //}
     }
 }
---
+++ problems/IPCTrainers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problems
+{
+    public class IPCTrainers
+    {
+        public void CallTrainers()
+        {
+            int cases = Convert.ToInt32(Console.ReadLine());
+            int[,] data = new int[cases, 2];
+            for (int i = 0; i < cases; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+
+            }
+        }
+    }
+}
