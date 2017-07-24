412704b208e47f9b606804d66465274316ae9a88
RajatChaudhari <silverprince.v1@gmail.com>
2017-07-24 23:31:20 +0530

    CcAccepted

---
+++ problems/SecurityPatrol.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problems
+{
+    class SecurityPatrol
+    {
+        /// <summary>
+        /// They start from a checkpoint and go in opposite direction for greater coverage.
+        /// Isha starts with speed U metre/second and Suhana starts with speed V metre/second. 
+        /// They both have a walkie-talkie, operating for maximum distance of X metres. How long will they be able to communicate ?
+        /// </summary>
+        void TwoGirls()
+        {
+            //reading no of test cases
+            int cases = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < cases; i++)
+            {
+                //reading the speed of both girls and walikie talkie Range
+                string[] U = Console.ReadLine().Split(' ');
+                double[] V = Array.ConvertAll(U, double.Parse);
+
+                double A = V[2]/(V[0] + V[1]);
+                Console.WriteLine(A);
+            }
+        }
+        //static void Main(string[] args)
+        //{
+        //    SecurityPatrol call = new SecurityPatrol();
+        //    call.TwoGirls();
+        //}
+    }
+}
---
+++ problems/Traverse.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problems
+{
+    class Traverse
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        void CallTraverse()
+        {
+            int cases = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < cases;i++ )
+            {
+                int M = Convert.ToInt32(Console.ReadLine());
+                int x = 0;
+                int y = 0;
+                int count=0;
+                for (int j = 0; j < M;)
+                {
+                    if (count%2==0)
+                    {
+                        if (j < M)
+                        {
+                            x++;
+                            j++;
+                        }
+                        
+                    }
+                    else
+                    {
+                        if (j < M)
+                        {
+                            y++;
+                            j++;
+                        }
+                        if(j<M)
+                        {
+                            y++;
+                            j++; 
+                        }
+                        
+                    }
+                    count++;
+                }
+                
+                Console.WriteLine(x+" "+y);
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Traverse tObj = new Traverse();
+            tObj.CallTraverse();
+            Console.Read();
+        }
+    }
+}
