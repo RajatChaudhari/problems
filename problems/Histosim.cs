2ab25ee047c94d5f2cddc6eeb9b93f4505c4dfca
RajatChaudhari <silverprince.v1@gmail.com>
2017-07-25 22:30:58 +0530

    CCIncomplete

---
+++ problems/Histosim.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problems
+{
+    class Histosim
+    {
+        void CheckString()
+        {
+            int cases = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < cases;i++ )
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                string P = input[0];//.ToCharArray();
+                string Q = input[1];//.ToCharArray();
+                char curr='a';
+                string fromQ;
+                List<char> charlist = new List<char>();
+                for(int a=0;a<Q.Length;a++)
+                {
+                    fromQ = Q.Substring(a,1);
+                    if(!P.Contains(fromQ))
+                    {
+                        P.Replace(P.Substring(a, 1), fromQ);
+                    }
+                }
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Histosim call=new Histosim();
+            call.CheckString();
+        }
+    }
+}
