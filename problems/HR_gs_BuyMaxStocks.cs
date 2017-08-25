9b7960b9d96976f01e95244466ad357531f4ae88
RajatChaudhari <silverprince.v1@gmail.com>
2017-08-25 23:02:52 +0530

    HR

---
+++ problems/HR_gs_BuyMaxStocks.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problems
+{
+    class HR_gs_BuyMaxStocks
+    {
+        static long buyMaximumProducts(int n, long k, int[] a)
+        {
+            long stock=0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                long amount = 0;
+                long cc = k / a[i - 1];
+                long min = Math.Min(cc, i);
+                stock += min;
+                amount += min * a[i - 1];
+                k -= amount;
+            }
+            return stock;
+        }
+
+        //static void Main(String[] args)
+        //{
+        //    int n = Convert.ToInt32(Console.ReadLine());
+        //    string[] arr_temp = Console.ReadLine().Trim(' ').Split(' ');
+        //    int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        //    long k = Convert.ToInt64(Console.ReadLine());
+        //    long result = buyMaximumProducts(n, k, arr);
+        //    Console.WriteLine(result);
+        //    Console.ReadLine();
+        //}
+
+
+        #region 1sttry
+        //static long buyMaximumProducts(int n, long k, int[] a)
+        //{
+        //    long stock = 0;
+
+        //    for (int i = 1, j = 0; i <= n; i++, j++)
+        //    {
+        //        int amount = 0;
+        //        amount = i * a[j];
+        //        while (amount > k)
+        //        {
+        //            i--;
+        //            amount = i * a[j];
+        //        }
+        //        if (i > 0)
+        //        {
+        //            k -= amount;
+        //            stock += i;
+        //        }
+
+        //        i = j + 1;
+
+
+        //    }
+        //    return stock;
+        //} 
+        #endregion
+    }
+}
---
+++ problems/HR_gs_TimeSeries.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problems
+{
+    class HR_gs_TimeSeries
+    {
+        static void Main(String[] args)
+        {
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int q = Convert.ToInt32(tokens_n[1]);
+            string[] t_temp = Console.ReadLine().Split(' ');
+            int[] time = Array.ConvertAll(t_temp, Int32.Parse);
+            string[] p_temp = Console.ReadLine().Split(' ');
+            int[] price = Array.ConvertAll(p_temp, Int32.Parse);
+            for (int a0 = 0; a0 < q; a0++)
+            {
+                string[] tokens__type = Console.ReadLine().Split(' ');
+                int _type = Convert.ToInt32(tokens__type[0]);
+                int v = Convert.ToInt32(tokens__type[1]);
+
+                if(_type==1)
+                {
+
+                }
+            }
+
+
+        }
+    }
+}
