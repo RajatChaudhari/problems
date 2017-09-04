15d18bbbbaf7cfa3d3ffe95e9c773ba1cb5bf32f
RajatChaudhari <silverprince.v1@gmail.com>
2017-09-04 11:30:42 +0530

    less space

--- problems/Chefsum.cs
+++ problems/Chefsum.cs
@@ -15,45 +15,19 @@ namespace problems
             {
                 int n = Convert.ToInt32(Console.ReadLine());
                 int[] nums = Array.ConvertAll(Console.ReadLine().Trim(' ').Split(' '), int.Parse);
-                Dictionary<int, int> sumsDict = new Dictionary<int, int>();
                 long[] sums = new long[n];
-                int[] pre = new int[n];
-                int[] suf = new int[n];
                 int ans = 0;
                 for (int j = 0; j < n; j++)
                 {
-                    nums.CopyTo(pre, j);
-                    sums[j] = prefixSum(j,nums) + suffixSum(j,nums);
-                }
-                
+                    int[] pre = new int[n];
+                    int[] suf = new int[n];
+                    Array.Copy(nums, 0, pre, 0,j+1);
+                    Array.Copy(nums, j, suf, 0, n - j);
+                    sums[j] = pre.Sum() + suf.Sum();
+                }   
                 ans = Array.IndexOf(sums,sums.Min());
                 Console.WriteLine(ans+1);
             }
-            Console.ReadLine();
-
-        }
-
-        public static long prefixSum(int j, int[] nums)
-        {
-            long sum = 0;
-
-            //for (int i = 0; i <= j; i++)
-            //{
-            //    sum += nums[i];
-            //}
-            Array.ForEach(nums, delegate (int i) { sum += i; });
-            return sum;
-        }
-
-        public static long suffixSum(int j,int[] nums)
-        {
-            int last = nums.Length;
-            long sum = 0;
-            for (int i = j; i < last; i++)
-            {
-                sum += nums[i];
-            }
-            return sum;
         }
     }
 }
